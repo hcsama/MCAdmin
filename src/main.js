4f097d99e5599303999eb790ce42867250c2a5a5
Hans-Christian Fuchs <hans.christian.fuchs@googlemail.com>
2020-06-10 23:27:36 +0200

    remove zingrid

--- src/main.js
+++ src/main.js
@@ -3,13 +3,6 @@ import App from './App.vue'
 
 Vue.config.productionTip = false;
 
-// import the zingchart-vue component, which in turn, imports the zingchart library itself.
-import zingchartVue from 'zingchart-vue';
-Vue.component('zingchart', zingchartVue);
-
-// Import the ZingGrid library, By default, the ZingGrid library registers itself as a web component.
-import ZingGrid from "zinggrid";
-
 new Vue({
   render: h => h(App),
 }).$mount('#app');
